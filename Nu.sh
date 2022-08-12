dc9aa9b96dd1921786ec77579fd79fb46e04e635
Githhubrts <110772544+Githhubrts@users.noreply.github.com>
2022-08-12 22:27:39 +0530

    Create Nu.sh

---
+++ Nu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "enter number "
+read n1
+if [ $n1 = 2 ]; then
+echo "enter nu is $n1"
+elif [ $n1 = 4 ]; then
+echo "your enter no is $n1"
+else
+echo "your enter no is not 2 or 4"
+fi
